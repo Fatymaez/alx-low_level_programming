86ed9c9e9a3b79e1b8231f4a807cc21a6e610fd1
fatimaezzahra <fatima.ezzahra9123@gmail.com>
2023-03-19 17:12:27 -0700

    puts sentence

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/*
+ * write a c program that prints an exact sentence
+ */
+
+int main(void)
+{
+	puts("Programming is like building a multilingual puzzle/n");
+
+    return (0);
+}	
